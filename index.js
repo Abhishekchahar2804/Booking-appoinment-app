d9349434c716a89b3c57b85cadccf4360eb191b1
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-05-31 14:19:33 +0530

    now we can also delete from the cru crud website

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const listItem = document.getElementById('users');
 let form = document.getElementById('my-form');
 
 form.addEventListener('submit', storeInfo);
-listItem.addEventListener('click', removeItem);
+
 
 function storeInfo(e) {
     e.preventDefault();
@@ -29,10 +29,20 @@ function storeInfo(e) {
 
 }
 
-function newUser(key) {
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get('https://crudcrud.com/api/91444e7c3330458b82961530a3cf0df9/appoinment')
+        .then((res)=>{
+            for(var i=0;i<res.data.length;i++){
+                
+                newUser(res.data[i]);
+            }
+        })
+})
+
+function newUser(obj) {
     let newItem = document.createElement('li');
     newItem.className = 'item';
-    newItem.appendChild(document.createTextNode(key));
+    newItem.appendChild(document.createTextNode(obj.name+" "+obj.email));
 
     let newBtn1 = document.createElement('button');
     newBtn1.className = 'delete';
@@ -40,28 +50,32 @@ function newUser(key) {
 
     newItem.appendChild(newBtn1);
 
+    newBtn1.onclick=(e)=>{
+        let li = e.target.parentElement;
+        //localStorage.removeItem(li.firstChild.textContent);
+        var id = obj._id;
+        console.log(id);
+        axios.delete('https://crudcrud.com/api/91444e7c3330458b82961530a3cf0df9/appoinment/'+id)
+            .then((res)=>{})
+            .catch((err)=>console.log(err));
+        listItem.removeChild(li);
+    }
+
     let newBtn2 = document.createElement('button');
     newBtn2.className = 'edit';
     newBtn2.appendChild(document.createTextNode('edit'));
+    newItem.appendChild(newBtn2);
 
     newBtn2.onclick = (e) => {
         let li = e.target.parentElement;
-        localStorage.removeItem(li.firstChild.textContent);
+        //localStorage.removeItem(li.firstChild.textContent);
+        
         listItem.removeChild(li);
         document.getElementById('name').value = obj.name;
         document.getElementById('email').value = obj.email;
     }
 
-    newItem.appendChild(newBtn2);
-
     listItem.appendChild(newItem);
 }
 
-function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
-        let li = e.target.parentElement;
-        localStorage.removeItem(li.firstChild.textContent);
-        listItem.removeChild(li);
 
-    }
-}
