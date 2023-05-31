434ad471d8f5ed772feb7259d06fec181a8a04a4
Abhishekchahar2804 <105548044+Abhishekchahar2804@users.noreply.github.com>
2023-05-31 13:31:56 +0530

    Now adding data to crud crud not on local storage

--- index.js
+++ index.js
@@ -4,55 +4,64 @@ const listItem = document.getElementById('users');
 
 let form = document.getElementById('my-form');
 
-form.addEventListener('submit',storeInfo);
-listItem.addEventListener('click',removeItem);
+form.addEventListener('submit', storeInfo);
+listItem.addEventListener('click', removeItem);
 
-function storeInfo(e){
+function storeInfo(e) {
     e.preventDefault();
-    
-    const userName=document.querySelector('#name').value;
-    const userEmail = document.querySelector('#email').value;
-    let obj={
-        name:userName,
-        email:userEmail
-    }
-    let key = userName+" "+userEmail;
-    let myObj=JSON.stringify(obj);
-    localStorage.setItem(key,myObj);
 
+    const userName = document.querySelector('#name').value;
+    const userEmail = document.querySelector('#email').value;
+    let obj = {
+        name: userName,
+        email: userEmail
+    }
+    let key = userName + " " + userEmail;
+    axios.post('https://crudcrud.com/api/91444e7c3330458b82961530a3cf0df9/appoinment', obj)
+        .then((res) => {
+            newUser(key);
+            console.log(res.data)
+        })
+
+        .catch((err) => console.log(err));
+    // let myObj=JSON.stringify(obj);
+    // localStorage.setItem(key,myObj);
+
+}
+
+function newUser(key) {
     let newItem = document.createElement('li');
-    newItem.className='item';
+    newItem.className = 'item';
     newItem.appendChild(document.createTextNode(key));
 
-    let newBtn1= document.createElement('button');
-    newBtn1.className='delete';
+    let newBtn1 = document.createElement('button');
+    newBtn1.className = 'delete';
     newBtn1.appendChild(document.createTextNode('delete'));
 
     newItem.appendChild(newBtn1);
 
-    let newBtn2= document.createElement('button');
-    newBtn2.className='edit';
+    let newBtn2 = document.createElement('button');
+    newBtn2.className = 'edit';
     newBtn2.appendChild(document.createTextNode('edit'));
 
-    newBtn2.onclick=(e) =>{
+    newBtn2.onclick = (e) => {
         let li = e.target.parentElement;
         localStorage.removeItem(li.firstChild.textContent);
         listItem.removeChild(li);
-        document.getElementById('name').value=obj.name;
-        document.getElementById('email').value=obj.email;
+        document.getElementById('name').value = obj.name;
+        document.getElementById('email').value = obj.email;
     }
 
     newItem.appendChild(newBtn2);
 
     listItem.appendChild(newItem);
-
 }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
         let li = e.target.parentElement;
         localStorage.removeItem(li.firstChild.textContent);
         listItem.removeChild(li);
-        
+
     }
 }
